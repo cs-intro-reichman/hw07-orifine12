23220b8ed356153724de5662eb25ecd9021bc024
Ori Fine <ori.fine@post.runi.ac.il>
2026-01-01 19:03:01 +0200

    v1.11

--- IsSorted.java
+++ IsSorted.java
@@ -1,10 +1,15 @@
 public class IsSorted {
 
-    private static boolean isSorted(int[] a, int i) {
-    if (a.length <= 1 || i == a.length - 1) return true;
-    return a[i] <= a[i + 1] && isSorted(a, i + 1);
-}
     
+    public static boolean isSorted(int[] a) {
+        return isSorted(a, 0);
+    }
+
+    // Helper recursive function
+    private static boolean isSorted(int[] a, int i) {
+        if (a.length <= 1 || i >= a.length - 1) return true;
+        return a[i] <= a[i + 1] && isSorted(a, i + 1);
+    }
 
     public static void main(String[] args) {
         int[] a = {1, 2, 2, 5};
@@ -12,5 +17,5 @@ public class IsSorted {
 
         int[] b = {1, 3, 2};
         System.out.println(isSorted(b)); // false
-    
+    }
 }
